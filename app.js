2ca575c33140cf54c8cb158db64c33efa0a4e5ea
Unat Pannu <unatkaur.pannu@bcah.christuniversity.in>
2025-02-28 13:50:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+var app = angular.module("talentApp", []);
+app.controller("TalentController", function ($scope, $http) {
+    $scope.sortBy = "name";
+    $scope.limit = 6;
+    $scope.selectedTalent = null;
+    $http.get("talents.json").then(function (response) {
+        $scope.talents = response.data;
+    });
+    $scope.$watch("selectedTalent", function (newVal, oldVal) {
+        if (newVal !== oldVal && newVal !== null) {
+            console.log("Talent selected:", newVal);
+            $scope.$broadcast("talentSelected", newVal);
+        }
+    });
+    $scope.openModal = function (talent) {
+        $scope.selectedTalent = talent;
+        $scope.$emit("modalOpened", talent);
+    };
+    $scope.closeModal = function () {
+        $scope.selectedTalent = null;
+    };
+    $scope.$on("talentSelected", function (event, data) {
+        console.log("Talent Selected Event Received:", data);
+    });
+    $scope.$on("modalOpened", function (event, data) {
+        console.log("Modal Opened for:", data.name);
+    });
+});
